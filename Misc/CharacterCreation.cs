a2b87d2785bcab5069a297bbedbec6d108e52153
James <james.srn@gmail.com>
2012-05-26 16:06:40 +0200

    Set skills cap to something absurdly high

--- Misc/CharacterCreation.cs
+++ Misc/CharacterCreation.cs
@@ -645,6 +645,7 @@ namespace Server.Misc
 			newChar.Hue = newChar.Race.ClipSkinHue( args.Hue & 0x3FFF ) | 0x8000;
 
 			newChar.Hunger = 20;
+			newChar.Skills.Cap = 100000;
 
 			bool young = false;
 
