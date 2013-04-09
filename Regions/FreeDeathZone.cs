7201e45bbf764dfafc9eea2e6351e7e0bf113041
James <james.srn@gmail.com>
2013-04-09 20:12:46 +0100

    The base region for the free death zone
    This is the zone described in ticket #6 where PvP deaths gain no injury
    points

---
+++ Regions/FreeDeathZone.cs
@@ -0,0 +1,20 @@
+using Server;
+using Server.Mobiles;
+
+namespace Server.Regions
+{
+	public class FreeDeathZone : BaseRegion
+	{
+		
+		public override void OnEnter( Mobile m )
+		{
+			
+		}
+
+		public override void OnExit( Mobile m )
+		{
+			
+		}
+
+	}
+}
