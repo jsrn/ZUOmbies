7a38de18e579bf40a70dc60e6e4e2ce8f2abb339
James <james.srn@gmail.com>
2013-04-09 21:08:38 +0100

    Added overload and fixed bracing

--- Regions/FreeDeathZone.cs
+++ Regions/FreeDeathZone.cs
@@ -5,17 +5,21 @@ namespace Server.Regions
 {
 	public class FreeDeathZone : BaseRegion
 	{
+
+		public FreeDeathZone( XmlElement xml, Map map, Region parent ) : base( xml, map, parent )
+		{
+		}
 		
 		public override void OnEnter( Mobile m )
 		{
-			if( typeof( m ) == typeof( PlayerMobile ){
+			if( typeof( m ) == typeof( PlayerMobile ) ){
 				m.SetFreeDeaths(true);
 			}
 		}
 
 		public override void OnExit( Mobile m )
 		{
-			if( typeof( m ) == typeof( PlayerMobile ){
+			if( typeof( m ) == typeof( PlayerMobile ) ){
 				m.SetFreeDeaths(false);
 			}
 		}
