ad1c4075c490bf217eb2f3ad3964e2c66fa4a2de
James <james.srn@gmail.com>
2013-05-14 17:19:22 +0200

    Permakill!

--- Custom/DeathCountCommand.cs
+++ Custom/DeathCountCommand.cs
@@ -14,12 +14,13 @@ namespace Server.Scripts.Commands
 		public static void Initialize()
 		{
 			CommandSystem.Register( "DeathCount", AccessLevel.Player, new CommandEventHandler( DeathCount_OnCommand ) );
-			CommandSystem.Register( "ResetDeathCount", AccessLevel.Administrator, new CommandEventHandler( ResetDeathCount_OnCommand ) );
+			CommandSystem.Register( "ResetDeathCount", AccessLevel.GameMaster, new CommandEventHandler( ResetDeathCount_OnCommand ) );
+			CommandSystem.Register( "Permakill", AccessLevel.GameMaster, new CommandEventHandler( Permakill_OnCommand ) );
+
 		}
 
 		[Usage( "DeathCount" )]
 		[Description( "Tells the player how many death points they have." )]
-
 		private static void DeathCount_OnCommand( CommandEventArgs e )
 		{
 			DeathCountSystem.StartPopPouch( e.Mobile );
@@ -27,10 +28,16 @@ namespace Server.Scripts.Commands
 		
 		[Usage( "ResetDeathCount" )]
 		[Description( "Sets the targetted player's death count to 0." )]
-		
 		private static void ResetDeathCount_OnCommand( CommandEventArgs e )
 		{
 			DeathCountSystem.ResetDeathCount( e.Mobile );
+		}
+		
+		[Usage( "Permakill" )]
+		[Description( "Permakill the targetted mobile." )]
+		private static void Permakill_OnCommand( CommandEventArgs e )
+		{
+			DeathCountSystem.Permakill( e.Mobile );
 		}
 	}
 }
