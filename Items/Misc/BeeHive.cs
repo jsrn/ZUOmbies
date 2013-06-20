cd0073a483262a25d0b7a93a24d4cfc155410bf4
James Nutt <james.srn@gmail.com>
2013-06-20 23:43:19 +0100

    Beehive carries honey

--- Items/Misc/BeeHive.cs
+++ Items/Misc/BeeHive.cs
@@ -24,6 +24,12 @@ namespace Server.Items
 		{
 			if( m_IsFull )
 			{
+				if( Utility.RandomDouble() < 0.2 )
+				{
+					from.SendMessage( "You gather some beeswax and put it in your pack." );
+					from.Backpack.DropItem( new Beeswax() );
+				}
+
 				from.SendMessage( "You gather some honey from the bee hive and put it in a jar." );
 				from.Backpack.DropItem( new JarHoney() );
 				m_IsFull = false;
