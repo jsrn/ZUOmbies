b198c20d8d800a7bb60acfc66eb33f2e029f2387
James Nutt <james.srn@gmail.com>
2013-06-30 20:30:15 +0100

    Spirit speak costs evil unless next to corpse

--- Skills/SpiritSpeak.cs
+++ Skills/SpiritSpeak.cs
@@ -3,6 +3,7 @@ using Server;
 using Server.Items;
 using Server.Spells;
 using Server.Network;
+using Server.Mobiles;
 
 namespace Server.SkillHandlers
 {
@@ -107,62 +108,77 @@ namespace Server.SkillHandlers
 			{
 				Corpse toChannel = null;
 
+				int cost = 10;
+
 				foreach ( Item item in Caster.GetItemsInRange( 3 ) )
 				{
 					if( item is Corpse && !( (Corpse)item ).Channeled && !( (Corpse)item ).Animated )
 					{
+						cost = 0;
 						toChannel = (Corpse)item;
 						break;
 					}
 				}
 
-				int max, min, mana, number;
+				if( ( Caster is PlayerMobile && ((PlayerMobile)Caster).EvilPoints >= cost ) || !(Caster is PlayerMobile) )
+				{
+					int max, min, mana, number;
 
-				if ( toChannel != null )
-				{
-					min = 1 + (int)(Caster.Skills[SkillName.SpiritSpeak].Value * 0.25);
-					max = min + 4;
-					mana = 0;
-					number = 1061287; // You channel energy from a nearby corpse to heal your wounds.
-				}
-				else
-				{
-					min = 1 + (int)(Caster.Skills[SkillName.SpiritSpeak].Value * 0.25);
-					max = min + 4;
-					mana = 10;
-					number = 1061286; // You channel your own spiritual energy to heal your wounds.
-				}
-
-				if ( Caster.Mana < mana )
-				{
-					Caster.SendLocalizedMessage( 1061285 ); // You lack the mana required to use this skill.
-				}
-				else
-				{
-					Caster.CheckSkill( SkillName.SpiritSpeak, 0.0, 120.0 );
-
-					if ( Utility.RandomDouble() > (Caster.Skills[SkillName.SpiritSpeak].Value / 100.0) )
+					if ( toChannel != null )
 					{
-						Caster.SendLocalizedMessage( 502443 ); // You fail your attempt at contacting the netherworld.
+						min = 1 + (int)(Caster.Skills[SkillName.SpiritSpeak].Value * 0.25);
+						max = min + 4;
+						mana = 0;
+						number = 1061287; // You channel energy from a nearby corpse to heal your wounds.
 					}
 					else
 					{
-						if ( toChannel != null )
-						{
-							toChannel.Channeled = true;
-							toChannel.Hue = 0x835;
-						}
-
-						Caster.Mana -= mana;
-						Caster.SendLocalizedMessage( number );
-
-						if ( min > max )
-							min = max;
-
-						Caster.Hits += Utility.RandomMinMax( min, max );
-
-						Caster.FixedParticles( 0x375A, 1, 15, 9501, 2100, 4, EffectLayer.Waist );
+						min = 1 + (int)(Caster.Skills[SkillName.SpiritSpeak].Value * 0.25);
+						max = min + 4;
+						mana = 10;
+						number = 1061286; // You channel your own spiritual energy to heal your wounds.
 					}
+
+					if ( Caster.Mana < mana )
+					{
+						Caster.SendLocalizedMessage( 1061285 ); // You lack the mana required to use this skill.
+					}
+					else
+					{
+						Caster.CheckSkill( SkillName.SpiritSpeak, 0.0, 120.0 );
+
+						if ( Utility.RandomDouble() > (Caster.Skills[SkillName.SpiritSpeak].Value / 100.0) )
+						{
+							Caster.SendLocalizedMessage( 502443 ); // You fail your attempt at contacting the netherworld.
+						}
+						else
+						{
+							if ( toChannel != null )
+							{
+								toChannel.Channeled = true;
+								toChannel.Hue = 0x835;
+							}
+
+							Caster.Mana -= mana;
+							Caster.SendLocalizedMessage( number );
+
+							if ( min > max )
+								min = max;
+
+							Caster.Hits += Utility.RandomMinMax( min, max );
+
+							Caster.FixedParticles( 0x375A, 1, 15, 9501, 2100, 4, EffectLayer.Waist );
+						}
+					}
+
+					if ( Caster is PlayerMobile )
+					{
+						((PlayerMobile)Caster).EvilPoints -= cost;
+					}
+				}
+				else
+				{
+					Caster.SendMessage( "You reach out to the spirits, but they do not respond." );
 				}
 
 				FinishSequence();
