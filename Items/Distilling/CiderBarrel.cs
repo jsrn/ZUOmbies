6c57021807bbce4a312cdf29a12242ba46e5ebc5
James Nutt <james.srn@gmail.com>
2013-07-07 18:15:13 +0100

    Booze barrels!

---
+++ Items/Distilling/CiderBarrel.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace Server.Items
+{
+	public class CiderBarrel : Container
+	{
+		public override bool IsDecoContainer
+		{
+			get{ return false; }
+		}
+
+		[Constructable]
+		public CiderBarrel() : base( 0xE77 )
+		{
+			Name = "a cider barrel";
+			Movable = false;
+		}
+
+		public CiderBarrel( Serial serial ) : base( serial )
+		{
+		}
+
+		public override bool OnDragDrop( Mobile from, Item dropped )
+		{
+			if ( !base.OnDragDrop( from, dropped ) )
+				return false;
+
+			MutateFermentable( from, dropped );
+
+			return true;
+		}
+
+		public override bool OnDragDropInto( Mobile from, Item item, Point3D p )
+		{
+			if ( !base.OnDragDropInto( from, item, p ) )
+				return false;
+
+			MutateFermentable( from, item );
+
+			return true;
+		}
+
+		private void MutateFermentable( Mobile from, Item item )
+		{
+			if ( item is Apple )
+			{
+				item.Delete();
+				for ( int i = 0; i < ((Apple)item).Amount; i++ )
+				{
+					Item beverage = new Jug( BeverageType.Cider ); // 0 == ale
+					AddItem( beverage );
+				}
+				from.SendMessage( "You add the materials to the barrel, and retrieve some of the older stock." );
+			}
+			else
+			{
+				from.SendMessage( "That cannot be used to make anything." );
+			}
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+	}
+}
---
+++ Items/Distilling/WineBarrel.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace Server.Items
+{
+	public class WineBarrel : Container
+	{
+		public override bool IsDecoContainer
+		{
+			get{ return false; }
+		}
+
+		[Constructable]
+		public WineBarrel() : base( 0xE77 )
+		{
+			Name = "a wine barrel";
+			Movable = false;
+		}
+
+		public WineBarrel( Serial serial ) : base( serial )
+		{
+		}
+
+		public override bool OnDragDrop( Mobile from, Item dropped )
+		{
+			if ( !base.OnDragDrop( from, dropped ) )
+				return false;
+
+			MutateFermentable( from, dropped );
+
+			return true;
+		}
+
+		public override bool OnDragDropInto( Mobile from, Item item, Point3D p )
+		{
+			if ( !base.OnDragDropInto( from, item, p ) )
+				return false;
+
+			MutateFermentable( from, item );
+
+			return true;
+		}
+
+		private void MutateFermentable( Mobile from, Item item )
+		{
+			if ( item is Grapes )
+			{
+				item.Delete();
+				for ( int i = 0; i < ((Grapes)item).Amount; i++ )
+				{
+					Item beverage = new BeverageBottle( BeverageType.Wine ); // 0 == ale
+					AddItem( beverage );
+				}
+				from.SendMessage( "You add the materials to the barrel, and retrieve some of the older stock." );
+			}
+			else
+			{
+				from.SendMessage( "That cannot be used to make anything." );
+			}
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+	}
+}
