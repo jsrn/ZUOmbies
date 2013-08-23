348d3a06b44ff244390eabbd52875ab7f705bfd6
James Nutt <james.srn@gmail.com>
2013-08-23 20:49:28 +0100

    Stash deed

--- Items/Containers/HiddenStash.cs
+++ Items/Containers/HiddenStash.cs
@@ -1,6 +1,8 @@
 using System;
+using Server.Mobiles;
+using Server.Targeting;
 using Server;
-using Server.Network;
+using Server.Engines.Craft;
 
 namespace Server.Items
 {
@@ -10,23 +12,23 @@ namespace Server.Items
 
 		public TimeSpan ConcealPeriod
 		{
-			get{ return TimeSpan.FromSeconds( 5.0 ); }
+			get{ return TimeSpan.FromSeconds( 15.0 ); }
 		}
 
-		public int IsValidLocation()
+		public bool IsValidLocation()
 		{
 			return IsValidLocation( GetWorldLocation(), Map );
 		}
 
-		public int IsValidLocation( Point3D p, Map m )
+		public bool IsValidLocation( Point3D p, Map m )
 		{
 			if ( m == null )
-				return 502956; // You cannot place a trap on that.
+				return false;
 
 			if ( TileAlreadyContainsStash( p, m ) )
-				return 502956; // You cannot place a trap on that.
+				return false;
 
-			return 0;
+			return true;
 		}
 
 		private bool TileAlreadyContainsStash( Point3D location, Map map )
@@ -102,6 +104,101 @@ namespace Server.Items
 
 			if ( Visible )
 				BeginConceal();
+		}
+	}
+
+	public class StashDeed : Item, ICraftable
+	{
+		[Constructable]
+		public StashDeed() : base( 0x14F0 )
+		{
+			Name = "a hidden stash";
+			Weight = 1.0;
+		}
+
+		public StashDeed( Serial serial ) : base( serial )
+		{
+		}
+
+		public HiddenStash Construct( Mobile from )
+		{
+			try{ return Activator.CreateInstance( typeof ( HiddenStash ) ) as HiddenStash; }
+			catch{ return null; }
+		}
+
+		public override void OnDoubleClick( Mobile from )
+		{
+			if ( IsChildOf( from.Backpack ) )
+			{
+				from.Target = new InternalTarget( this );
+				from.SendMessage( "Where would you like to hide this stash?" );
+			}
+			else
+				from.SendLocalizedMessage( 1042001 ); // That must be in your pack for you to use it.
+		}
+
+		public override void Serialize( GenericWriter writer )
+		{
+			base.Serialize( writer );
+
+			writer.Write( (int) 0 ); // version
+		}
+
+		public override void Deserialize( GenericReader reader )
+		{
+			base.Deserialize( reader );
+
+			int version = reader.ReadInt();
+		}
+
+		#region ICraftable Members
+		public int OnCraft( int quality, bool makersMark, Mobile from, CraftSystem craftSystem, Type typeRes, BaseTool tool, CraftItem craftItem, int resHue )
+		{
+			ItemID = 0x14F0;
+			return 1;
+		}
+		#endregion
+
+		private class InternalTarget : Target
+		{
+			private StashDeed m_Deed;
+
+			public InternalTarget( StashDeed deed ) : base( -1, true, TargetFlags.None )
+			{
+				m_Deed = deed;
+			}
+
+			protected override void OnTarget( Mobile from, object targeted )
+			{
+				IPoint3D p = targeted as IPoint3D;
+				Map map = from.Map;
+
+				if ( p == null || map == null || m_Deed.Deleted )
+					return;
+
+				if ( m_Deed.IsChildOf( from.Backpack ) )
+				{
+					HiddenStash stash = m_Deed.Construct( from );
+
+					if ( stash == null )
+						return;
+
+					if ( !stash.IsValidLocation( new Point3D( p.X, p.Y, p.Z ), from.Map ) )
+					{
+						from.SendMessage( "You cannot hide a stash there." );
+						stash.Delete();
+						return;
+					}
+
+					from.SendMessage( "You carefully conceal the stash." );
+					stash.MoveToWorld( new Point3D( p.X, p.Y, p.Z ), from.Map );
+					m_Deed.Delete();
+				}
+				else
+				{
+					from.SendLocalizedMessage( 1042001 ); // That must be in your pack for you to use it.
+				}
+			}
 		}
 	}
 }
